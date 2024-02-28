d63c592d1e4d05902a7f0e55cec50e1a27930a21
melchaubaroux <mel.chaubaroux@gmail.com>
2024-02-28 16:29:54 +0100

    retake

--- asset/config - Copie.py
+++
@@ -1,6 +0,0 @@
-
-
-
-headers = {"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiODAwMTFhYmEtYzNlNy00YzY5LTliZGMtMGQ5OThkODk2OWU1IiwidHlwZSI6ImFwaV90b2tlbiJ9.N3jUN6ucg324ejC2D9BYZNTAi0awJ4BWdLsXqva1164"}
-
-url = "https://api.edenai.run/v2/text/chat"
